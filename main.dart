24c02e98046904ada200cf682a2548ebf3c30b6a
Marcus Schmidt <nightmare240219871987@gmail.com>
2025-06-19 11:01:43 +0200

    Testing Future

--- main.dart
+++ main.dart
@@ -1,3 +1,16 @@
-void main() {
-  print("hello World");
+import 'dart:core';
+import 'dart:io';
+
+Future<void> main() async {
+  print("Fetching User Order ...");
+  print(await createOrderMessage());
+}
+
+Future<String> fetchUserOrder() {
+  return Future.delayed(const Duration(seconds: 5), () => 'Large Latte');
+}
+
+Future<String> createOrderMessage() async {
+  var order = await fetchUserOrder();
+  return "Your oder is : $order";
 }
